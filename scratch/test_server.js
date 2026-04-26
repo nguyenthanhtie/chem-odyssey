156ee26632c59ae2ff97a9f3f9453e5041fa9de4
nguyenthanhtie <nguyenthanhtien2120@gmail.com>
2026-04-26 19:41:00 +0700

    Dọn dẹp cấu trúc thư mục: Xóa các file test, script nháp và SQL tạm thời đã qua sử dụng

--- scratch/test_server.js
+++
@@ -1,8 +0,0 @@
-import http from 'http';
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ status: 'ok', msg: 'Simple server works' }));
-});
-server.listen(5000, '127.0.0.1', () => {
-  console.log('Simple server running on http://127.0.0.1:5000');
-});
--- scripts/sql/fix_balancing_unique.sql
+++
@@ -1,2 +0,0 @@
--- SQL: Add unique constraint to balancing_questions
-ALTER TABLE public.balancing_questions ADD CONSTRAINT unique_equation_string UNIQUE (equation_string);
